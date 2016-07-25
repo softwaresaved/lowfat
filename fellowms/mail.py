85d2a0d6518827955cddd1cba8d78a6ced021cb1
Raniere Silva <raniere@rgaiacs.com>
2016-07-25 16:28:23 +0100

    Update email

--- fellowms/mail.py
+++ fellowms/mail.py
@@ -15,12 +15,12 @@ def new_event_notification(event):
     # Email to admin
     mail_admins("New event request",
             "Please review {}.".format(
-                reverse_full("event_detail", args=[event.id])),
+                reverse_full("event_review", args=[event.id])),
                 fail_silently=False,
             )
 
     # Email to user
-    send_mail('Your new event',
+    send_mail('Your event request was received',
             'You can check the information of your event at {}.'.format(
                 reverse_full("event_detail", args=[event.id])),
             DEFAULT_FROM_EMAIL,
@@ -32,14 +32,14 @@ def new_expense_notification(expense):
     # Email to admin
     mail_admins("New expense submited",
             "Please process {}.".format(
-                reverse_full("expense_detail", args=[expense.pk])),
+                reverse_full("expense_review", args=[expense.id])),
                 fail_silently=False,
             )
 
     # Email to user
     send_mail('Your new expense claim was received',
             'You can check the information of your expense claim at {}.'.format(
-                reverse_full("expense_detail", args=[expense.pk])),
+                reverse_full("expense_detail", args=[expense.id])),
             DEFAULT_FROM_EMAIL,
             [expense.event.fellow.email],
             fail_silently=False
@@ -49,14 +49,14 @@ def new_blog_notification(blog):
     # Email to admin
     mail_admins("New blog request",
             "Please review {}.".format(
-                reverse_full("event_detail", args=[blog.pk])),
+                reverse_full("event_review", args=[blog.id])),
                 fail_silently=False,
             )
 
     # Email to user
-    send_mail('Your new event',
+    send_mail('Your new blog post was received',
             'You can check the information of your event at {}.'.format(
-                reverse_full("event_detail", args=[blog.pk])),
+                reverse_full("event_detail", args=[blog.id])),
             DEFAULT_FROM_EMAIL,
             [blog.event.fellow.email],
             fail_silently=False
