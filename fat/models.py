91fd3860aa8a42b91ae1e9023272886a1684edf7
Raniere Silva <raniere@rgaiacs.com>
2016-08-08 11:06:42 +0100

    Add permission to advertise the event

--- fat/models.py
+++ fat/models.py
@@ -232,6 +232,8 @@ class Event(models.Model):
             default=0.00)    
     justification = models.TextField()
     additional_info = models.TextField(blank=True)
+    can_be_advertise_before = models.BooleanField(default=False)
+    can_be_advertise_after = models.BooleanField(default=False)
 
     # Admin fields
     ad_status = models.CharField(choices=AD_STATUS,
