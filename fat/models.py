06c274fbdcca5a690997745d4d1b7c0a508e727c
Raniere Silva <raniere@rgaiacs.com>
2016-08-09 15:22:05 +0100

    Add recipient for expenses

--- fat/models.py
+++ fat/models.py
@@ -313,6 +313,10 @@ class Expense(models.Model):
             decimal_places=2,
             blank=False,
             default=0.00)
+    recipient = models.TextField(
+        blank=True,
+        help_text="Keep empty if the recipient is the fellow."
+    )
     final = models.BooleanField(
         default=False,
         help_text="This is your last expense claim for the event"
