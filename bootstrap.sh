f4cf1e31f9f8a1e3a1920a36153c6b3091120cfa
Raniere Silva <raniere@rgaiacs.com>
2016-08-17 13:45:01 +0100

    Add font-awesome to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,28 @@
 
 STATIC_FOLDER=fat/static/
 TMP_FOLDER=/tmp/
-ZIP_FILE=/tmp/bootstrap.zip
+BOOTSTRAP_ZIP=/tmp/bootstrap.zip
+FONT_AWESOME_ZIP=/tmp/fontawesome.zip
 
+# Bootstrap
 curl \
     -L \
-    -o $ZIP_FILE \
+    -o $BOOTSTRAP_ZIP \
     https://github.com/twbs/bootstrap/releases/download/v3.3.6/bootstrap-3.3.6-dist.zip
 unzip \
-    $ZIP_FILE \
+    $BOOTSTRAP_ZIP \
     -d $TMP_FOLDER
+
+# Font Awesome
+curl \
+    -L \
+    -o $FONT_AWESOME_ZIP \
+    https://github.com/FortAwesome/Font-Awesome/archive/v4.6.3.zip
+unzip \
+    $FONT_AWESOME_ZIP \
+    -d $TMP_FOLDER
+
+# Copy files
 for folder in css fonts js
 do
     cp -r $TMP_FOLDER/*/$folder $STATIC_FOLDER
