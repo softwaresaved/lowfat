be1d35c2e059f20d8f1f326c74877794f0e60eaf
Raniere Silva <raniere@rgaiacs.com>
2017-02-14 18:56:52 +0000

    Disable email notification before row out

--- lowfat/views.py
+++ lowfat/views.py
@@ -241,7 +241,7 @@ def fund_form(request):
         if formset.is_valid():
             fund = formset.save()
             messages.success(request, 'Funding request saved on our database.')
-            new_fund_notification(fund)
+            #new_fund_notification(fund)
 
             # Default value for budget_approved is budget_total.
             # The reason for this is to save staffs to copy and paste the approved amount.
@@ -316,7 +316,7 @@ def fund_review(request, fund_id):
                 }
             )
             mail.save()
-            fund_review_notification(mail)
+            #fund_review_notification(mail)
             return HttpResponseRedirect(
                 reverse('fund_detail', args=[fund.id,])
             )
@@ -386,7 +386,7 @@ def expense_form(request):
     if formset.is_valid():
         expense = formset.save()
         messages.success(request, 'Expense saved on our database.')
-        new_expense_notification(expense)
+        #new_expense_notification(expense)
         return HttpResponseRedirect(
             reverse('expense_detail', args=[expense.id,])
         )
@@ -452,7 +452,7 @@ def expense_review(request, expense_id):
                 }
             )
             mail.save()
-            expense_review_notification(mail)
+            #expense_review_notification(mail)
             return HttpResponseRedirect(
                 reverse('expense_detail', args=[expense.id,])
             )
@@ -489,7 +489,7 @@ def blog_form(request):
     if formset.is_valid():
         blog = formset.save()
         messages.success(request, 'Blog draft saved on our database.')
-        new_blog_notification(blog)
+        #new_blog_notification(blog)
         return HttpResponseRedirect(
             reverse('blog_detail', args=[blog.id,])
         )
@@ -549,7 +549,7 @@ def blog_review(request, blog_id):
                 }
             )
             mail.save()
-            blog_review_notification(mail)
+            #blog_review_notification(mail)
             return HttpResponseRedirect(
                 reverse('blog_detail', args=[blog.id,])
             )
