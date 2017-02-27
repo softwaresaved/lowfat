0248037390c5ee2ce64d1ce4422381ac146770d9
Raniere Silva <raniere@rgaiacs.com>
2017-02-27 07:58:27 +0000

    Change LANGUAGE_CODE to en-gb

--- lowfat/settings.py
+++ lowfat/settings.py
@@ -148,7 +148,7 @@ SOCIAL_AUTH_GITHUB_SECRET = ''
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
