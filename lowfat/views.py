26a54037986024f706b8bf1823c6fca9dfbb2557
Raniere Silva <raniere@rgaiacs.com>
2017-05-09 14:07:04 +0100

    Fix query for fellow profile

--- lowfat/views.py
+++ lowfat/views.py
@@ -204,36 +204,40 @@ def claimant_detail(request, claimant_id):
         'expenses_status': expenses_status,
         'blogs_status': blogs_status,
         'claimant': this_claimant,
-        'blogs': Blog.objects.filter(
-            Q(author=this_claimant) | Q(coauthor=this_claimant)
-        ),
     }
 
-    if request.user.is_authenticated() and request.user.is_superuser:
-        funds = Fund.objects.filter(
-            claimant=this_claimant,
-            status__in=funding_requests_status
-        )
-        context.update(
-            {
-                'funds': pair_fund_with_blog(funds, "P"),
-                'expenses': Expense.objects.filter(
-                    fund__claimant=this_claimant,
-                    status__in=expenses_status
-                ),
-            }
-        )
-    else:
-        funds = Fund.objects.filter(
-            claimant=this_claimant,
-            can_be_advertise_after=True,
-            status__in=["A", "F"]
-        )
-        context.update(
-            {
-                'funds': pair_fund_with_blog(funds, "P"),
-            }
-        )
+    if request.user.is_authenticated():
+        if request.user.is_superuser:
+            funds = Fund.objects.filter(
+                claimant=this_claimant,
+                status__in=funding_requests_status
+            )
+            context.update(
+                {
+                    'funds': pair_fund_with_blog(funds, "P"),
+                    'expenses': Expense.objects.filter(
+                        fund__claimant=this_claimant,
+                        status__in=expenses_status
+                    ),
+                    'blogs': Blog.objects.filter(
+                        Q(author=this_claimant, status=blogs_status) | Q(coauthor=this_claimant, status=blogs_status)
+                    ).distinct(),
+                }
+            )
+        else:
+            funds = Fund.objects.filter(
+                claimant=this_claimant,
+                can_be_advertise_after=True,
+                status__in=funding_requests_status
+            )
+            context.update(
+                {
+                    'funds': pair_fund_with_blog(funds, "P"),
+                    'blogs': Blog.objects.filter(
+                        Q(author=this_claimant, status=blogs_status) | Q(coauthor=this_claimant, status=blogs_status)
+                    ).distinct(),
+                }
+            )
 
     return render(request, 'lowfat/claimant_detail.html', context)
 
