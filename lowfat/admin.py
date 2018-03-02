185d7d67caa6ffb161306595f91f75967c2a93c1
Raniere Silva <raniere@rgaiacs.com>
2018-03-02 16:38:49 +0000

    Split Claimant edit in admin view into sections

--- lowfat/admin.py
+++ lowfat/admin.py
@@ -6,6 +6,70 @@ from .models import *
 
 @admin.register(Claimant)
 class ClaimantAdmin(SimpleHistoryAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "user",
+                    "forenames",
+                    "surname",
+                    "email",
+                ]
+            },
+        ),
+        (
+            "Personal Information",
+            {
+                "fields": [
+                    "phone",
+                    "gender",
+                    "home_country",
+                    "home_city",
+                    "home_lon",
+                    "home_lat",
+                    "photo",
+                ]
+            },
+        ),
+        (
+            "Professional information",
+            {
+                "fields": [
+                    "career_stage_when_apply",
+                    "job_title_when_apply",
+                    "research_area",
+                    "research_area_code",
+                    "affiliation",
+                    "department",
+                    "group",
+                    "funding",
+                    "funding_notes",
+                ],
+            },
+        ),
+        (
+            "Administration",
+            {
+                "fields": [
+                    "slug",
+                    "terms_and_conditions",
+                    "application_year",
+                    "inauguration_grant_expiration",
+                    "received_offer",
+                    "fellow",
+                    "collaborator",
+                    "is_into_training",
+                    "carpentries_instructor",
+                    "research_software_engineer",
+                    "claimantship_grant",
+                    "attended_inaugural_meeting",
+                    "attended_collaborations_workshop",
+                    "notes_from_admin",
+                ],
+            },
+        ),
+    ]
     list_display = [
         'forenames',
         'surname',
