b9390be7ed5cfcef50a8480a08c928a66771d182
James Graham <j.graham@software.ac.uk>
2021-06-14 16:32:38 +0100

    fix(deploy): run `bootstrap.sh` at container start

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 # Ensure database migrations are applied
 python3 manage.py migrate
+
+# Ensure staticfiles are available
+bash /app/bootstrap.sh
 python3 manage.py collectstatic --no-input
 
 exec $@
