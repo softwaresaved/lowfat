2a145d0ff0c364cdbdb39b76a134356c79b13b85
James Graham <j.graham@software.ac.uk>
2021-06-15 12:35:15 +0100

    style: exclude star imports from linter in inits
    Star imports are more acceptable in `__init__.py` files as they ease
    transition from a single module to a package

--- lowfat/views/__init__.py
+++ lowfat/views/__init__.py
@@ -1,4 +1,6 @@
-from .blog import *
-from .claimant import *
-from .expense import *
-from .fund import *
+# Star imports used here as they allow easy migration from
+# a single views.py to a views package
+from .blog import *  # noqa
+from .claimant import *  # noqa
+from .expense import *  # noqa
+from .fund import *  # noqa
