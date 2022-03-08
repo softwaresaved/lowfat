2fe05afc26f74ab27fc94f329566b6f908cb37b5
James Graham <j.graham@software.ac.uk>
2022-03-08 13:44:17 +0000

    fix: serve staticfiles in development

--- lowfat/urls.py
+++ lowfat/urls.py
@@ -16,6 +16,7 @@ Including another pathconf
 from django.urls import include, path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 from . import views
@@ -120,4 +121,4 @@ urlpatterns = [  # pylint: disable=invalid-name
     path('staff/', include(STAFF_PATTERNS)),
     path('admin/', admin.site.urls),
     path('index/', views.index, name="index"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
