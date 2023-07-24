27e5fca08a9b18b066c30ff5ed643f1d71ee8fd9
phillybroadbent <phillybroadbent@gmail.com>
2023-07-24 17:15:49 +0100

    The amount committed to a funding request  remains even if there is overspend on other funding requests

--- lowfat/models/claimant.py
+++ lowfat/models/claimant.py
@@ -439,9 +439,11 @@ class Claimant(models.Model):
             grant_heading="F"
         )
 
+        # Minus sum of amount approved not amount claimed
+        
         spent_from_committed = 0
         for fund in this_claimant_funds:
-            spent_from_committed += sum([expense.amount_claimed for expense in Expense.objects.filter(
+            spent_from_committed += sum([expense.fund.budget_approved for expense in Expense.objects.filter(
                 fund=fund,
                 status__in=['A', 'M', 'F']
             )])
